66f23a55f7b1c088207b5ddd614f453f1979a96f
Hackeem Bassaragh <hackeem@Hackeems-Air.home>
2022-02-25 17:50:05 -0500

    first commit

---
+++ app/src/main/java/com/example/lab4_02_24_2022/AllImages.kt
@@ -0,0 +1,19 @@
+package com.example.lab4_02_24_2022
+
+class AllImages {
+    public val allImages = arrayOf(
+        "https://images.metmuseum.org/CRDImages/ep/web-large/DT1562.jpg",
+        "https://images.metmuseum.org/CRDImages/ep/web-large/DT1565.jpg",
+        "https://images.metmuseum.org/CRDImages/ep/web-large/DT1566.jpg",
+        "https://images.metmuseum.org/CRDImages/ep/web-large/DT1567.jpg",
+        "https://images.metmuseum.org/CRDImages/ep/web-large/DT1568.jpg",
+    )
+    public val allImageCaptions = arrayOf(
+        "The Monet Family in Their Garden at Argenteuil - 1874 - by a French artiste, Edouard Manet ", //62
+        "The Rehearsal of the Ballet Onstage - 1874 - by a French artiste, Edgar Degas", //65
+        "A Woman Seated beside a Vase of Flowers (Madame Paul Valpinçon?) - 1865 - by a French artiste, Edgar Degas ", //66
+        "Wheat Field with Cypresses - 1889 - by a Dutch artiste, Vincent van Gogh", //67
+        "Irises - 1890 - by a Dutch Artiste, Vincent van Gogh "
+    )
+
+}
